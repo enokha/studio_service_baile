86c0ee07ad6bed8213cdf6a28a085927cea360f2
enokha <enokha@gmail.com>
2024-07-09 02:23:59 +0200

    finish docker composer settings!

--- studio_service/studio_service/urls.py
+++ studio_service/studio_service/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from studios.views import StudioViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'studios', StudioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Add this line
 ]
